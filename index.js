598806389061f7e705e89bed806d8975d604ec34
dhanush <dhanushkamath@gmail.com>
2020-10-31 03:01:50 +0530

    simple api

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+const {home , allBooks,specificBook,addbook,deletebook} = require("./router")
+
+//home route
+
+app.get("/",home);
+
+//all books route
+app.get("/allbooks",allBooks);
+
+//specific book route
+app.get("/book/:name",specificBook);
+
+app.post("/addbook",addbook);
+
+app.delete("/delete/:name",deletebook);
+
+
+app.listen(9000,()=>{
+
+    console.log("running...");
+
+})
+
---
+++ router.js
@@ -0,0 +1,101 @@
+const fs = require("fs");
+const fileName = "./books.json";
+
+
+
+
+exports.home = (req , res)=>{
+    res.status(200).send("welcome friend");
+}
+
+exports.allBooks = (req , res)=>{
+    fs.readFile(fileName , (err , data)=>{
+        if (err){
+          return  res.status(404).send("error occured");
+        }
+
+       const books = JSON.parse(data);
+        res.send(books);
+    })
+    
+}
+
+exports.specificBook = (req,res)=>{
+    fs.readFile(fileName , (err , data)=>{
+        if (err){
+          return  res.status(404).send("error occured");
+        }
+
+        var books = JSON.parse(data);
+
+        for (let i = 0; i < books.length; i++) {
+            if (books[i].book.bookname == req.params.name){
+                return res.status(200).send(books[i].book);
+            }
+        }
+        return res.status(200).send("book not found");
+    });
+   
+}
+
+exports.addbook = (req , res)=>{
+    //get books from req
+    var inputBook = req.body;
+   
+    //validate the input 
+    if((inputBook.book.bookname !="" && inputBook.book.bookname != undefined) && 
+    (inputBook.book.bookprice !="" && inputBook.book.bookprice != undefined)){
+        
+        //reading the file 
+        fs.readFile(fileName,(err,data)=>{
+            if (err) throw err;
+            var books = JSON.parse(data);
+            // check if the file exists 
+            for (var i = 0; i < books.length; i++) {
+                
+                if(books[i].book.bookname.toString() === inputBook.book.bookname.toString()){
+                    return res.send("already exists");
+                }
+            }
+            
+            //will be executed if the file does not already exist
+            books.push(inputBook)
+            fs.writeFile(fileName,JSON.stringify(books),(err)=>{if (err) throw err; res.send("book added to DB")});
+            
+        });
+    }
+}
+
+
+exports.deletebook = (req , res)=>{
+    var bookToDel = req.params.name;
+
+    fs.readFile(fileName,(err,data)=>{
+        if (err) throw err;
+        var books = JSON.parse(data);
+        var book1 = [];
+        // check if the file exists 
+        for (var i = 0; i < books.length; i++) {
+            
+            if(books[i].book.bookname.toString() === bookToDel.toString()){
+                delete books[i];
+
+                for(var j = 0 ; j < books.length ; j++){
+                    if (books[j] == null){
+                        continue;
+                    }
+                    book1.push(books[j]);
+                }
+                fs.writeFile(fileName,JSON.stringify(book1),(err)=>{if (err) throw err;});
+                return res.send("book deleted");
+            }
+        }
+        return res.send("book not found");
+    });
+
+
+
+}
+
+
+
